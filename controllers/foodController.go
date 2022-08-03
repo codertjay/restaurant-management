4a70e7c2a0c896a43352e3f23dcd051db23ebc41
Codertjay <codertjay@gmail.com>
2022-08-03 02:22:54 +0100

    controller

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,12 +1,29 @@
 package controller
 
 import (
+	"context"
+	"restaurant-management/database"
+	"restaurant-management/models"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+		var food models.Food
+		err :=foodCollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
+		if err != nil{
+			c.Json(http.StatusInternalServerError,gin.M{"error":"Error occured fetching the food item"})
+		}
+		c.Json(http.StatusOk,food)
 	}
 }
 
